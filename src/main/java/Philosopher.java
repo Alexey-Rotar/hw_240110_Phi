99072bfd332120355c35dd99daf2dd644c0ed2e5
alexey.rotar <ar_mail@inbox.ru>
2024-01-12 00:10:26 +0500

    1

--- src/main/java/Philosopher.java
+++ src/main/java/Philosopher.java
@@ -4,40 +4,56 @@ public class Philosopher extends Thread {
     private String name;
     private Fork leftFork;
     private Fork rightFork;
-    //private Random random;
+    boolean isThinking;
+    boolean isEating;
+    int count = 2;
 
     public Philosopher(String name, Fork leftFork, Fork rightFork){
         this.name = name;
         this.leftFork = leftFork;
         this.rightFork = rightFork;
-        //random = new Random();
     }
 
     @Override
     public void run(){
         try {
-            synchronized (leftFork){
-                eat();
+            while (count > 0){
+                if (!leftFork.getState() && !rightFork.getState())
+                    eat();
+                else
+                    think();
             }
-            think();
-        } catch (InterruptedException e){
-            e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void eat() throws InterruptedException{
         leftFork.setState(true);
         rightFork.setState(true);
+        isEating = true;
         System.out.println(name + " ест вилками " + leftFork.getName() +  " и " + rightFork.getName());
         Thread.sleep((long) (Math.random() * 1000));
+        count--;
         leftFork.setState(false);
         rightFork.setState(false);
         System.out.println(name + " поел, вилки " + leftFork.getName() +  " и " + rightFork.getName() + " освободились" );
+        isEating = false;
     }
 
     public void think() throws InterruptedException{
-        System.out.println(name + " думает...");
-        Thread.sleep((long) (Math.random() * 1000));
-        System.out.println(name + " закончил думать");
+        if (isEating){
+            isThinking = false;
+        }
+        if (!isThinking && !isEating){
+            System.out.println(name + " думает...");
+            isThinking = true;
+        }
+        if (!isThinking){
+            isThinking = true;
+        }
+
+        //System.out.println(name + " закончил думать");
+        //System.out.println(name + " думает...");
     }
 }
